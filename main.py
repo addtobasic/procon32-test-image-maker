5dc77d90514630b0b7988cb30c1f3d778c658002
Genshi0916 <genshi0916@gmail.com>
2021-05-09 14:10:26 +0900

    :sparkles: 縦横の分割数と画像だけで分割,結合できるようにした

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import numpy as np
 import cv2
 import os
-
 img = cv2.imread("testImage/procon_logo.png")
 
-wSplitNum = 2
-hSplitNum = 2
+wSplitNum = 3
+hSplitNum = 3
 
 # 分割後の横の長さのpx
 w_split_size = img.shape[0] // wSplitNum
@@ -13,28 +12,28 @@ w_split_size = img.shape[0] // wSplitNum
 # 分割後の縦の長さのpx
 h_split_size = img.shape[1] // hSplitNum
 
-v_size = img.shape[0] // w_split_size * w_split_size
+w_size = img.shape[0] // w_split_size * w_split_size
 h_size = img.shape[1] // h_split_size * h_split_size
 
-img = img[:v_size, :h_size]
+img = img[:w_size, :h_size]
 
-out_img = []
-[out_img.extend(np.hsplit(h_img, wSplitNum))
-  for h_img in np.vsplit(img, hSplitNum)]
+split_img = []
+[split_img.extend(np.hsplit(h_img, wSplitNum)) for h_img in np.vsplit(img, hSplitNum)]
 
-# shuffle = np.random.permutation(range(len(img)))
-# img = np.vstack((
-#     # np.hstack(img[shuffle[0:5]]),
-#     # np.hstack(img[shuffle[5:10]]),
-#     # np.hstack(img[shuffle[10:15]]),
-# ))
+num = 0
+comb_img = []
 
-# cv2.imwrite("./outImage/test.jpg",img)
+for num in range(hSplitNum):
+  comb_img.append(np.hstack(split_img[wSplitNum*num:wSplitNum*(num+1)]))
+
+comb_img = np.vstack(comb_img)
 
 if not os.path.exists("./outImage"):
   os.mkdir("./outImage")
 
+cv2.imwrite("./outImage/test.jpg",comb_img)
+
 count = 0
-for outImage in out_img:
-  cv2.imwrite("./outImage/img-"+str(count)+".jpg",out_img[count])
+for outImage in split_img:
+  cv2.imwrite("./outImage/img-"+str(count)+".jpg",split_img[count])
   count += 1
